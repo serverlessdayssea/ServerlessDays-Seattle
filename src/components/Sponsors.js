6d712d62d5ba228a27b80d55bbf3390a1be6f7c6
Matt Weagle <mweagle@gmail.com>
2018-09-24 14:34:59 -0700

    Add IOPipe, Stackery sponsors

--- src/components/Sponsors.js
+++ src/components/Sponsors.js
@@ -14,6 +14,12 @@ class Sponsors extends Component {
             <Col s={12} m={6} l={6} className='center-align top-marg'>
               <img src='/media/ibm.png' className='responsive-img not-wide'></img>
             </Col>
+            <Col s={12} m={6} l={6} className='center-align top-marg'>
+              <img src='/media/stackery-large-stacked-teal-small-w.png' className='responsive-img not-wide'></img>
+            </Col>
+            <Col s={12} m={6} l={6} className='center-align top-marg'>
+              <img src='/media/io_logo_black_rgb_600_600.png' className='responsive-img not-wide'></img>
+            </Col>
           </Row>
           <div className='space-foot' />
           <hr/>
